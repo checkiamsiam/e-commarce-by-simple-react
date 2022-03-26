69369d76466ab5988e6797cd6dc29016901b9377
checkiamsiam <issiam02415@gmail.com>
2022-03-26 18:37:33 +0600

    responsive added

--- src/components/QA/QA.js
+++ src/components/QA/QA.js
@@ -9,7 +9,7 @@ const QA = () => {
       <div className="blog-container">
         <div className="react blog">
           <h1>How React Works?</h1>
-          <p>Ans: react is a javascript library. we use component to order react and react convert the conponent in 3 perameter of a function and this build in react function use a duplicate dom and then merge it to original dom react works in one way data binding process</p>
+          <p>Ans: react is a javascript library. we use component to order react and react convert the conponent in 3 perameter of a function and this build in react function use a duplicate dom and then merge it to original dom. react works in one way data binding process</p>
         </div>
         <div className="props-vs-state blog">
         <h1>Props vs state</h1>
